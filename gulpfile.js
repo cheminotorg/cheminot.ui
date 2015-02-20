363b4556db4ab8862c43bc1e97c6d8f2881a4007
Sebastien Renault <sre@zenexity.com>
2015-02-20 08:40:21 +0100

    Add task to generate loader.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ var rename = require("gulp-rename");
 var sketch = require("gulp-sketch");
 var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
+var exec = require('gulp-exec');
 var del = require('del');
 
 var fontName = 'cheminot';
 var template = 'fontawesome-style';
 
-gulp.task('fonts', ['clean'], function(){
+gulp.task('fonts', ['clean:fonts'], function(){
   gulp.src('cheminot.sketch')
     .pipe(sketch({
       export: 'artboards',
@@ -31,22 +32,43 @@ gulp.task('fonts', ['clean'], function(){
       var options = {
         glyphs: codepoints,
         fontName: fontName,
-        fontPath: '../fonts/',
+        fontPath: '../target/',
         className: 'icon-cheminot'
       };
       gulp.src('templates/' + template + '.css')
         .pipe(consolidate('lodash', options))
         .pipe(rename({ basename:fontName }))
-        .pipe(gulp.dest('dist/css/'));
+        .pipe(gulp.dest('dist/fonts/css/'));
 
       gulp.src('templates/' + template + '.html')
         .pipe(consolidate('lodash', options))
         .pipe(rename({ basename: 'fonts' }))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/fonts/'));
     })
-    .pipe(gulp.dest('dist/fonts/'));
+    .pipe(gulp.dest('dist/fonts/target'));
 });
 
-gulp.task('clean', function(cb) {
-  del(['dist'], cb);
+gulp.task('export:frames', function() {
+  return gulp.src('cheminot.sketch')
+    .pipe(sketch({
+      export: 'artboards',
+      formats: 'jpg',
+      items: ['frame1', 'frame2','frame3']
+    })).pipe(gulp.dest('dist/loader'));
 });
+
+gulp.task('loader', ['clean:loader', 'export:frames'], function(cb) {
+  return gulp.src('.')
+    .pipe(exec('convert -dispose none -dispose previous -delay 25 dist/loader/frame1.jpg dist/loader/frame2.jpg dist/loader/frame3.jpg -loop 0  dist/loader/cheminot_f4f7f9.gif'))
+    .pipe(exec.reporter());
+});
+
+gulp.task('clean:loader', function(cb) {
+  del(['dist/loader'], cb);
+});
+
+gulp.task('clean:fonts', function(cb) {
+  del(['dist/fonts'], cb);
+});
+
+gulp.task('clean', ['clean:loader', 'clean:fonts'], function() {});
